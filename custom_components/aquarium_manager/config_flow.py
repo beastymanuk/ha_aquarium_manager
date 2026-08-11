52886c70bb669e46b25c098769dc03fb2a217b35
root <root@a0d7b954-vscode.local.hass.io>
2026-08-11 13:21:28 +0300

    Initial Home Assistant integration skeleton

---
+++ custom_components/aquarium_manager/config_flow.py
@@ -0,0 +1,31 @@
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class AquariumManagerConfigFlow(
+    config_entries.ConfigFlow,
+    domain=DOMAIN,
+):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Aquarium Manager",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "aquarium_name",
+                        default="My Aquarium",
+                    ): str,
+                }
+            ),
+        )
---
+++ custom_components/aquarium_manager/const.py
@@ -0,0 +1,1 @@
+DOMAIN = "aquarium_manager"
